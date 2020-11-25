313986f6a8e061c94eff766123774c9d49a540a8
Andrey <achirkof@gmail.com>
2020-11-25 21:54:41 +0300

    Remove v23 and v28 layouts

--- app/src/main/java/com/achirkof/movieapp/MainActivity.kt
+++ app/src/main/java/com/achirkof/movieapp/MainActivity.kt
@@ -2,7 +2,6 @@ package com.achirkof.movieapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.WindowManager
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
