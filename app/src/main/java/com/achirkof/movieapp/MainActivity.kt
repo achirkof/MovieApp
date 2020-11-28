4a4c9f267b17bd31ac06d8dc2bcc4566a052edeb
Andrey <achirkof@gmail.com>
2020-11-28 17:03:09 +0300

    Make a fragment for MovieList

--- app/src/main/java/com/achirkof/movieapp/MainActivity.kt
+++ app/src/main/java/com/achirkof/movieapp/MainActivity.kt
@@ -3,9 +3,9 @@ package com.achirkof.movieapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MovieDetails : AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.fmt_movie_list)
     }
 }
