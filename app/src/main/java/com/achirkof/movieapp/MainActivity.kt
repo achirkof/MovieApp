eb74dbcf57a2fecee12df704d6e836dcfd44edbb
Andrey <achirkof@gmail.com>
2020-12-02 11:49:42 +0300

    Movie item card

--- app/src/main/java/com/achirkof/movieapp/MainActivity.kt
+++ app/src/main/java/com/achirkof/movieapp/MainActivity.kt
@@ -2,10 +2,25 @@ package com.achirkof.movieapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.achirkof.movieapp.UI.MovieDetails
+import com.achirkof.movieapp.UI.MovieList
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.fmt_movie_list)
+        setContentView(R.layout.activity_main)
+
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .add(R.id.main_container, MovieList.newInstance())
+                .commit()
+        }
+    }
+
+    private fun onClick() {
+        supportFragmentManager.beginTransaction()
+            .addToBackStack(null)
+            .add(R.id.movie_details, MovieDetails.newInstance())
+            .commit()
     }
 }
